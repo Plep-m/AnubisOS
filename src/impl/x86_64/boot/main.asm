2a26158060501f97e567caf4f2194d1a2f6eb7f6
Plep-m <menut34@gmail.com>
2022-04-05 20:15:15 +0200

    adding more boiler plate code that i cant really explain myself

--- src/impl/x86_64/boot/main.asm
+++ src/impl/x86_64/boot/main.asm
@@ -4,6 +4,71 @@ global start
 section .text
 bits 32
 start:
+	mov esp, stack_top
+	call check_multiboot
+	call check_cpuid
+	call check_long_mode
 	mov dword [0xb8000], 0x2f4b2f4f
 	hlt
 
+check_multiboot:
+	cmp eax, 0xe36d76289
+	jne .no_multiboot
+	ret
+
+.no_multiboot:
+	move al, "M"
+	jmp error 
+
+check_cpuid:
+	pushfd
+	pop eax
+	mov ecx, eax
+	xor eax, 1 << 21
+	push eax 
+	popfd
+	pushfd 
+	pop eax
+	push ecx
+	popfd
+	cmp eax, ecx
+	je .no_cpuid
+	ret
+
+.no_cpuid:
+	mov al, "C"
+	jmp error
+
+
+
+
+check_long_mode:
+	mov eax, 0x80000000
+	cpuid
+	cmp eax, 0x80000001
+	jb .no_long_mode
+
+	mov eax, 0x80000001
+	cpuid
+	test edx, 1 << 29
+	jz .no_long_mode
+
+.no_long_mode:
+	mov al, "L"
+	jmp error
+
+error:
+	; print Error X where X is the error code 
+	mov dword [0xb8000], 0x4f524f45
+	mov dword [0xb8004], 0x4f3a4f52
+	mov dword [0xb8008], 0x4f204f20
+	mov dword [0xb800a], al
+	hlt 
+
+
+
+
+section .bss
+stack_bottom:
+	resb 4096 * 4
+stack_top:
